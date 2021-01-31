7248d7a42e2476d689738b70811fc3e0b1338fab
Thestickman391 <stickman391@gmail.com>
2021-01-31 15:49:14 +1300

    Correctly update values on reaction removal

--- index.js
+++ index.js
@@ -144,7 +144,15 @@ client.on('messageDelete', async (message) => {
     }
 });
 
-client.on('messageReactionAdd' || 'messageReactionRemove'.filter(filterReacts), async (reaction) => {
+client.on('messageReactionAdd', async (reaction) => {
+    updateVotes(reaction);
+});
+
+client.on('messageReactionRemove', async (reaction) => {
+    updateVotes(reaction);
+});
+
+async function updateVotes (reaction) {
 	// When we receive a reaction we check if the reaction is partial or not
 	if (reaction.partial) {
 		// If the message this reaction belongs to was removed the fetching might result in an API error, which we need to handle
@@ -190,6 +198,6 @@ client.on('messageReactionAdd' || 'messageReactionRemove'.filter(filterReacts), 
         }
 
     }
-});
+}
 
 client.login(process.env.discordToken);
